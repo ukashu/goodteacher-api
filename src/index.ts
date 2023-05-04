a6ececb122e3071666e9c790ba31d23d91d2762b
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-05-04 18:09:02 +0200

    changed ip for local testing

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,19 @@ import errorHandler from "./middleware/errorMiddleware.js"
 import 'dotenv/config'
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = Number(process.env.PORT) || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", sessionRoutes)
 
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
 app.use(errorHandler)
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log("listening on port ", PORT)
 });
